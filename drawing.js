b2c8e6a8a15ae4bd67bdc5536ed6673b721b83df
InsulatorGMan <76236444+InsulatorGMan@users.noreply.github.com>
2023-02-17 11:47:05 -0600

    Update drawing.js

--- drawing.js
+++ drawing.js
@@ -28,6 +28,23 @@ document.getElementById('brush').addEventListener('input', (ev) => {
     
 })
 
+function importData() {
+  let input = document.createElement('input');
+  input.type = 'file';
+  input.onchange = _ => {
+    // you can use this method to get file and perform respective operations
+            let files =   Array.from(input.files);
+            console.log(files);
+        };
+  input.click();
+  
+}
+
+document.querySelector(".load").addEventListener("click", () => {
+
+    importData();
+    
+});
 
 let clearBtn = document.querySelector(".clear")
 clearBtn.addEventListener("click", () => {
